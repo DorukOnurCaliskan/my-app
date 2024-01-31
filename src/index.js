3948ac33b037b337ad00f2fb24b1474de004b08c
Doruk Onur Caliskan <63496036+dorukcaliskan02@users.noreply.github.com>
2024-01-31 18:58:31 +0300

    a new prop version of book

--- src/index.js
+++ src/index.js
@@ -2,29 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-const Booklist = () => {
-  return <section className='booklist'>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-  </section>
-}
 const author = "Jordan Moore";
+const title = "How to Know a Person";
+const img = "https://images-na.ssl-images-amazon.com/images/I/81Gvt3v-J3L._AC_UL600_SR600,400_.jpg";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+    </section>
+  );
+}
 const Book = () => {
-  const title = "Interesting Facts For Curious Mindssssss";
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81Gvt3v-J3L._AC_UL600_SR600,400_.jpg"
-        alt="How to Know a Person"
-      />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
-
-      <h4>{author.toUpperCase()} </h4>
-      {/* <p>{let x = 6}</p> */}
-      <p>{6 + 6}</p>
+      <h4>{author} </h4>
     </article>
   );
 };
